feb3749308676b7464760ddece8696b6baf9460e
itsai <itsaizempoaltecatlmejia@gmail.com>
2019-10-18 18:01:08 -0700

    segundo commit

--- ControlLaboratorioElectronica/ControlLaboratorioElectronica/Form1.Designer.cs
+++ ControlLaboratorioElectronica/ControlLaboratorioElectronica/Form1.Designer.cs
@@ -30,6 +30,7 @@
 		{
 			this.components = new System.ComponentModel.Container();
 			this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+			this.panel1 = new System.Windows.Forms.Panel();
 			this.SuspendLayout();
 			// 
 			// bunifuElipse1
@@ -37,11 +38,20 @@
 			this.bunifuElipse1.ElipseRadius = 5;
 			this.bunifuElipse1.TargetControl = this;
 			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(57)))), ((int)(((byte)(106)))));
+			this.panel1.Location = new System.Drawing.Point(-1, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(183, 579);
+			this.panel1.TabIndex = 0;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(881, 578);
+			this.Controls.Add(this.panel1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "Form1";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -53,6 +63,7 @@
 		#endregion
 
 		private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+		private System.Windows.Forms.Panel panel1;
 	}
 }
 
